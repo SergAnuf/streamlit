06814b53206f49533c3c27935b7f55cea692b4bd
sergey <anufriiev.sergii@gmail.com>
2024-11-22 13:00:06 +0300

    add inputs option

--- plot.py
+++ plot.py
@@ -2,8 +2,11 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+m = st.number_input("Enter number of rows:", min_value=1, step=1)
+k = st.number_input("Enter number of columns:", min_value=2, step=1)
+
 map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    np.random.randn(m, k) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
 st.map(map_data)
